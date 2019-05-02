7cbd38af52a59546a681a152e4f69d67d00b5b78
alex m <amcnurlan@gmail.com>
2019-05-02 15:29:36 -0400

    Material UI Formik success

--- src/sign-in-form-2.jsx
+++ src/sign-in-form-2.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
+// import TextField from "@material-ui/core/TextField";
 
-import { Formik } from "formik";
+import { Formik, Field, Form } from "formik";
+import { TextField } from 'formik-material-ui';
 import * as Yup from "yup";
 
 import { Debug } from './formik-debug.jsx';
@@ -20,72 +21,54 @@ const initialValues = { name: "", email: "", password: "" };
 export default function SignInForm() {
   return (
     <Formik
-      render={props => (
-        <>
-          <Form {...props} />
-          {/* <Debug /> */}
-        </>
-      )}
+     
       initialValues={initialValues}
       validationSchema={validationSchema}
       onSubmit={(e) => {
         e.prevendDefault();
         alert('OK!!!')
       }}
-    />
+    >
+     { (props) => {
+        const {
+          handleSubmit,
+        } = props;
+      
+        console.log(props);
+        const margin = {marginTop: '5px', marginBottom: '5px'};
+      
+        return (
+          <Form>
+            <Field
+              name="email"
+              type="email"
+              label="Email"
+              component={TextField}
+              fullWidth
+            />
+             <Field
+              name="password"
+              type="password"
+              label="Password"
+              component={TextField}
+              fullWidth
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              style={{marginTop: '1rem'}}
+              onClick={handleSubmit}
+              fullWidth
+            >
+              Submit
+            </Button>
+
+            <Debug />
+          </Form>
+        );
+      }}
+    
+    </Formik>
   )
 }
-
-function Form(props) {
-  const {
-    values: { email, password },
-    errors,
-    touched,
-    handleSubmit,
-    handleChange,
-    handleBlur,
-  } = props;
-
-  console.log(props);
-  const margin = {marginTop: '5px', marginBottom: '5px'};
-
-  return (
-    <form onSubmit={() => {}}>
-      <TextField
-        id="email"
-        name="email"
-        label="Email"
-        value={email}
-        error={touched.email && errors.email}
-        helperText={errors.email}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        style={margin}
-        fullWidth
-      />
-      <TextField
-        id="password"
-        name="password"
-        label="Password"
-        type="password"
-        value={password}
-        error={touched.password && errors.password}
-        helperText={errors.password}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        style={margin}
-        fullWidth
-      />
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        style={{marginTop: '1rem'}}
-        onClick={handleSubmit}
-        fullWidth
-      >
-        Submit
-      </Button>
-    </form>
-  );
-};
